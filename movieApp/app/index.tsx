b2dcaa7ec4757ff6cde56455fc604d53010e1f07
Your Name <your.email@example.com>
2026-01-05 14:56:18 +0545

    basic setup

---
+++ movieApp/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+     className="flex-1 bg-pink-500 "
+    >
+      <Text className="text-3xl text-red-200">Welcome</Text>
+    </View>
+  );
+}
