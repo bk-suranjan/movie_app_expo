b462cc90e2a07c4a2d002d6b33d461af7a355da3
bk-suranjan <150880841+bk-suranjan@users.noreply.github.com>
2026-05-18 23:03:11 +0545

    Add comment to Index component in index.tsx

--- movieApp/app/index.tsx
+++ movieApp/app/index.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
+  // will this coment work or not
   return (
     <View
      className="flex-1 bg-pink-500 "
     >
       <Text>Tring</Text>
+      <Text>Learning</Text>
       <Text className="text-3xl text-red-200">Welcome</Text>
     </View>
   );
