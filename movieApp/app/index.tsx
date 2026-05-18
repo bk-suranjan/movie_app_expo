946dacb290f166e86763a0c5c9e0d166c7b1fded
bk-suranjan <150880841+bk-suranjan@users.noreply.github.com>
2026-05-18 23:01:58 +0545

    testing

--- movieApp/app/index.tsx
+++ movieApp/app/index.tsx
@@ -5,6 +5,7 @@ export default function Index() {
     <View
      className="flex-1 bg-pink-500 "
     >
+      <Text>Tring</Text>
       <Text className="text-3xl text-red-200">Welcome</Text>
     </View>
   );
